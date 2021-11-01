8d785c40a6e6640c39ecb68ff172d6d539cb0107
wolfwood <transmethyl@gmail.com>
2021-11-01 16:56:40 -0700

    initial checkin

---
+++ keyboards/handwired/tryadactyl/config.h
@@ -0,0 +1,207 @@
+/*
+Copyright 2021 wolfwood
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x0000
+#define DEVICE_VER   0x0001
+#define MANUFACTURER wolfwood
+#define PRODUCT      tryadactyl
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 8
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+//#define MATRIX_ROW_PINS { D0, D5 }
+//#define MATRIX_COL_PINS { F1, F0, B0 }
+
+#define XX NO_PIN
+
+#define DIRECT_PINS {		      \
+    {E0, C3, A4, A0, B6, B2, F0, F4}, \
+    {E1, C4, A5, A1, B5, B1, F1, F5}, \
+    {C0, C5, A6, A2, B4, B0, F2, F6}, \
+    {C1, C6, A7, A3, B3, E7, F3, F7}, \
+    {C2, C7, XX, XX, XX, XX, XX, XX}  \
+}
+
+#define UNUSED_PINS {B7, D3, D4, D7, E6} // D0, D1
+
+/* COL2ROW, ROW2COL */
+//#define DIODE_DIRECTION COL2ROW
+
+/*
+ * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
+ */
+#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+
+//#define LED_NUM_LOCK_PIN B0
+//#define LED_CAPS_LOCK_PIN B1
+//#define LED_SCROLL_LOCK_PIN B2
+//#define LED_COMPOSE_PIN B3
+//#define LED_KANA_PIN B4
+
+//#define BACKLIGHT_PIN B7
+//#define BACKLIGHT_LEVELS 3
+//#define BACKLIGHT_BREATHING
+
+//#define RGB_DI_PIN E2
+//#ifdef RGB_DI_PIN
+//#    define RGBLED_NUM 16
+//#    define RGBLIGHT_HUE_STEP 8
+//#    define RGBLIGHT_SAT_STEP 8
+//#    define RGBLIGHT_VAL_STEP 8
+//#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+//#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/*== all animations enable ==*/
+//#    define RGBLIGHT_ANIMATIONS
+/*== or choose animations ==*/
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_CHRISTMAS
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_RGB_TEST
+//#    define RGBLIGHT_EFFECT_ALTERNATING
+/*== customize breathing effect ==*/
+/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
+//#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
+/*==== use exp() and sin() ====*/
+//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
+//#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+//#define GRAVE_ESC_CTRL_OVERRIDE
+
+/*
+ * Force NKRO
+ *
+ * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
+ * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
+ * makefile for this to work.)
+ *
+ * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
+ * until the next keyboard reset.
+ *
+ * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
+ * fully operational during normal computer usage.
+ *
+ * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
+ * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
+ * bootmagic, NKRO mode will always be enabled until it is toggled again during a
+ * power-up.
+ *
+ */
+//#define FORCE_NKRO
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+/* Bootmagic Lite key configuration */
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
+
+
+
+#ifdef PS2_USE_USART
+    #define PS2_CLOCK_PORT  PORTD
+    #define PS2_CLOCK_PIN   PIND
+    #define PS2_CLOCK_DDR   DDRD
+    #define PS2_CLOCK_BIT   5
+    #define PS2_DATA_PORT   PORTD
+    #define PS2_DATA_PIN    PIND
+    #define PS2_DATA_DDR    DDRD
+    #define PS2_DATA_BIT    2
+
+    /* synchronous, odd parity, 1-bit stop, 8-bit data, sample at falling edge */
+    /* set DDR of CLOCK as input to be slave */
+    #define PS2_USART_INIT() do {   \
+        PS2_CLOCK_DDR &= ~(1<<PS2_CLOCK_BIT);   \
+        PS2_DATA_DDR &= ~(1<<PS2_DATA_BIT);     \
+        UCSR1C = ((1 << UMSEL10) |  \
+                  (3 << UPM10)   |  \
+                  (0 << USBS1)   |  \
+                  (3 << UCSZ10)  |  \
+                  (0 << UCPOL1));   \
+        UCSR1A = 0;                 \
+        UBRR1H = 0;                 \
+        UBRR1L = 0;                 \
+    } while (0)
+    #define PS2_USART_RX_INT_ON() do {  \
+        UCSR1B = ((1 << RXCIE1) |       \
+                  (1 << RXEN1));        \
+    } while (0)
+    #define PS2_USART_RX_POLL_ON() do { \
+        UCSR1B = (1 << RXEN1);          \
+    } while (0)
+    #define PS2_USART_OFF() do {    \
+        UCSR1C = 0;                 \
+        UCSR1B &= ~((1 << RXEN1) |  \
+                    (1 << TXEN1));  \
+    } while (0)
+    #define PS2_USART_RX_READY      (UCSR1A & (1<<RXC1))
+    #define PS2_USART_RX_DATA       UDR1
+    #define PS2_USART_ERROR         (UCSR1A & ((1<<FE1) | (1<<DOR1) | (1<<UPE1)))
+    #define PS2_USART_RX_VECT       USART1_RX_vect
+#endif
---
+++ keyboards/handwired/tryadactyl/tryadactyl.c
@@ -0,0 +1,40 @@
+/* Copyright 2021 wolfwood
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tryadactyl.h"
+
+#ifdef PS2_MOUSE_ENABLE
+#include "ps2_mouse.h"
+#include "ps2.h"
+#endif
+
+
+#ifdef PS2_MOUSE_ENABLE
+void ps2_mouse_init_user() {
+  /*
+    PS2_MOUSE_SEND(0xE2);
+    PS2_MOUSE_SEND(0x81);
+    PS2_MOUSE_SEND(0x4A);
+    PS2_MOUSE_SEND(0x59);
+
+
+  PS2_MOUSE_SEND(0xE2);
+  PS2_MOUSE_SEND(0x47);
+  PS2_MOUSE_SEND(0x2C);
+  PS2_MOUSE_SEND(0x01);
+  */
+}
+#endif
