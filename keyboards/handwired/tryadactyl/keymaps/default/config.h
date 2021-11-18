7d5e1173fe092077ae5a334b2392313b79987450
wolfwood <wolfwood@xomb.org>
2021-11-18 10:32:10 -0800

    auto mouse keys and scroll button

---
+++ keyboards/handwired/tryadactyl/keymaps/default/config.h
@@ -0,0 +1,5 @@
+#if defined MH_AUTO_BUTTONS
+  #define MH_AUTO_BUTTONS_LAYER _MBO
+  #define MH_AUTO_BUTTONS_TIMEOUT 5000
+  #define PS2_MOUSE_SCROLL_BTN_MASK (1<<PS2_MOUSE_BTN_MIDDLE) /* Default */
+#endif
--- keyboards/handwired/tryadactyl/keymaps/default/keymap.c
+++ keyboards/handwired/tryadactyl/keymaps/default/keymap.c
@@ -15,10 +15,20 @@
  */
 #include QMK_KEYBOARD_H
 
+// XXX figure out how to unify copy paste in firefox, terminal, tmux, emacs
+// XXX figure out how to unify paging in browser, terminal, tmux, man, less, emacs
+
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    // from DM: F# keys, numpad, [] {} and other displaced or harder to reach symbols
+    //_SYM,
+    // copypaste, pgup/down home end ? media/volume?
+    //_NAV,
+    // copypaste? mousekeys? scrolling?
+    //_MOUSE,
+    // Mouse Buttons Only (on thumbs leaving letters alone) stick copy/paste somewhere?
+    _MBO
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -32,19 +42,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_right_hand(
 		     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
 		     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-		     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+		     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  RCTL_T(KC_QUOT),
 		     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
 		               KC_UP,
 		     KC_LEFT,  KC_ENT,   KC_RGHT,
 		               KC_DOWN,
 		     KC_TILD,            KC_GRV,
-		     KC_ESC,   KC_ENT,   KC_TAB
+		     KC_GESC,  KC_ENT,   KC_TAB
     ),
     /*[_FN] = LAYOUT(
         QMKBEST, QMKURL,  _______,
             RESET,    XXXXXXX
 	    )*/
+    [_MBO] = LAYOUT_right_hand(
+		     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+		     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+		     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+		     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+		               KC_TRNS,
+		     KC_TRNS,  KC_TRNS,  KC_TRNS,
+		               KC_TRNS,
+		     KC_WBAK,            KC_WFWD,
+		     KC_BTN2,  KC_BTN1,  KC_BTN3
+    ),
 };
+
+#if defined MH_AUTO_BUTTONS && defined PS2_MOUSE_ENABLE && defined MOUSEKEY_ENABLE
+void mouse_mode(bool);
+
+static uint16_t mh_auto_buttons_timer;
+extern int tp_buttons; // mousekey button state set in action.c and used in ps2_mouse.c
+
+#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -65,5 +94,57 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
     }
+
+#if defined MH_AUTO_BUTTONS && defined PS2_MOUSE_ENABLE && defined MOUSEKEY_ENABLE
+    if (mh_auto_buttons_timer) {
+      switch (keycode) {
+      case KC_BTN1:
+      case KC_BTN2:
+      case KC_BTN3:
+      case KC_WBAK:
+      case KC_WFWD:
+	break;
+      default:
+	mouse_mode(false);
+      }
+    }
+#endif
     return true;
 }
+
+#if defined MH_AUTO_BUTTONS && defined PS2_MOUSE_ENABLE && defined MOUSEKEY_ENABLE
+void ps2_mouse_moved_user(report_mouse_t *mouse_report) {
+  if (mh_auto_buttons_timer) {
+    mh_auto_buttons_timer = timer_read();
+  } else {
+    if (!tp_buttons) {
+      mouse_mode(true);
+  #if defined CONSOLE_ENABLE
+      print("mh_auto_buttons: on\n");
+  #endif
+    }
+  }
+}
+
+void matrix_scan_user(void) {
+  if (mh_auto_buttons_timer && (timer_elapsed(mh_auto_buttons_timer) > MH_AUTO_BUTTONS_TIMEOUT)) {
+    if (!tp_buttons) {
+      mouse_mode(false);
+  #if defined CONSOLE_ENABLE
+      print("mh_auto_buttons: off\n");
+  #endif
+    }
+  }
+}
+
+void mouse_mode(bool on) {
+  if (on) {
+    layer_on(MH_AUTO_BUTTONS_LAYER);
+    mh_auto_buttons_timer = timer_read();
+  } else {
+    layer_off(MH_AUTO_BUTTONS_LAYER);
+    mh_auto_buttons_timer = 0;
+  }
+}
+
+#endif // defined MH_AUTO_BUTTONS && defined PS2_MOUSE_ENABLE && #defined MOUSEKEY_ENABLE
