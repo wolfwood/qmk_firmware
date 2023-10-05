92f441abcd13c6427a70560167fc8400920f7eba
wolfwood <transmethyl@gmail.com>
2023-10-05 11:35:08 -0500

    working minimal config for RP2040 and trackpoint on PIO1

---
+++ keyboards/handwired/rptp/config.h
@@ -0,0 +1,23 @@
+// Copyright 2023 wolfwood (@wolfwood)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+//#define PS2_MOUSE_INIT_DELAY 1000
+#define PS2_PIO_USE_PIO1
---
+++ keyboards/handwired/rptp/rptp.c
@@ -0,0 +1,11 @@
+#include "quantum.h"
+
+//debug_enable=true;
+
+/*void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  //debug_matrix=true;
+  //debug_keyboard=true;
+  debug_mouse=true;
+  }*/
