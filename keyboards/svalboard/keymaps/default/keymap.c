02a03d6be23f7c91cf47e924c55161fa6ac857b0
morganvenable <venable@gmail.com>
2023-11-04 06:34:11 -0700

    PFET, Trackpoint, gaze warp

--- keyboards/svalboard/keymaps/default/keymap.c
+++ keyboards/svalboard/keymaps/default/keymap.c
@@ -181,6 +181,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
           layer_clear();
           layer_on(NORMAL_HOLD);
+          SEND_STRING(SS_LCTL(SS_LALT(SS_LSFT(SS_TAP(X_A)))));
       } else {
           layer_off(NORMAL_HOLD);
       }
